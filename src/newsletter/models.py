9010837199303412e99c1600d8260f1498d91f50
Nasruddin <webo.geeky@gmail.com>
2015-12-15 23:18:09 +0530

    Initial commit

---
+++ src/newsletter/models.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class SignUp(models.Model):
+	email = models.EmailField()
+	full_name = models.CharField(max_length=120, blank=False, null=True)
+	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+	update = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+	def __unicode__(self): #python_3 is _str_ instead of _unicode_
+		return self.email
